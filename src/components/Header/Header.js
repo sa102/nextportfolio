40ef891ccfa6ca1113ae3765c56b6ace5e4ce09c
Saurav R <54012482+saurav-r-github@users.noreply.github.com>
2022-11-24 13:26:08 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>R.'s  Portfolio</span>
+          <DiCssdeck size="3rem" /> <span>Marvani Portfolio</span>
         </a>
       </Link>
     </Div1>
